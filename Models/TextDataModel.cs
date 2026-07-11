419c66894a52016f8a538f9a118058c9375b440f
椰汁 <cyan.01@outlook.com>
2026-07-11 13:29:58 +0800

    Add files via upload

---
+++ Models/TextDataModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Media;
+
+namespace ConvenientText.Models;
+
+public class TextDataModel : ObservableObject
+{
+    private string _displayText = "点击✎编辑文字";
+    private Color _textColor = Colors.White;
+    private double _fontSize = 18;
+
+    public string DisplayText
+    {
+        get => _displayText;
+        set => SetProperty(ref _displayText, value);
+    }
+
+    public Color TextColor
+    {
+        get => _textColor;
+        set => SetProperty(ref _textColor, value);
+    }
+
+    public double FontSize
+    {
+        get => _fontSize;
+        set => SetProperty(ref _fontSize, value);
+    }
+}
